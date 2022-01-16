e55fc96faf5a594b7ce9155962a5fe862f02f1c7
Hing <hing@live.com>
2022-01-16 14:37:55 +0800

    Add Chinese

--- src/l10n/locale.ts
+++ src/l10n/locale.ts
@@ -1,12 +1,14 @@
 //taken from https://github.com/valentine195/obsidian-leaflet-plugin/blob/master/src/l10n/locale.ts
 import en from "./locales/en";
 import de from "./locales/de";
+import zh from "./locales/zh";
 
 const locale = window.moment.locale;
 
 const localeMap: { [k: string]: Partial<typeof en> } = {
     en,
-    de
+    de,
+    zh
 };
 
 const userLocale = localeMap[locale()];
